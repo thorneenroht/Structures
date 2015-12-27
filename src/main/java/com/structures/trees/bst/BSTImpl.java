5f27594efcc38677a07b465cb441fe407abe3a24
thorneenroht <mattst9785@yahoo.com>
2015-12-27 12:48:59 -0500

    Added a unit test for insert null, added starter methods for depthfirstsearch and breadth first search.

--- src/main/java/com/structures/trees/bst/BSTImpl.java
+++ src/main/java/com/structures/trees/bst/BSTImpl.java
@@ -3,5 +3,31 @@ package com.structures.trees.bst;
 public class BSTImpl {
 	BSTNode head;
 	
+	public BSTImpl(){}
 	
+	
+	public void insert(BSTNode node){
+		if(node != null){
+			
+		}
+	}
+	
+	public int getSize(){
+		int i = 0;
+		if(head != null){
+			
+		}
+		return i;
+	}
+	
+	public BSTNode breadthFirstSearch(BSTNode node){
+		return null;
+	}
+	
+	public BSTNode depthFirstSearch(BSTNode node){
+		return null;
+	}
+	
+	
+
 }
--- src/test/java/com/structures/trees/bst/BSTImpleUnitTest.java
+++ src/test/java/com/structures/trees/bst/BSTImpleUnitTest.java
@@ -1,12 +1,23 @@
 package com.structures.trees.bst;
 
+import static org.junit.Assert.assertNull;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class BSTImpleUnitTest {
 	BSTNode node;
+	BSTImpl s = new BSTImpl();
 	
 	@Before
 	public void setup(){
 		node = new BSTNode(4);
+	}
+	
+	@Test
+	public void insertNullTest(){
+		s.insert(null);
+		
+		assertNull(s.head);
 	}
 }
