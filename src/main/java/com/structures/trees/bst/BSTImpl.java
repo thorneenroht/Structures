0b64b5b9adbc71e6c1161dc86a6c2beebb7ab5b7
thorneenroht <mattst9785@yahoo.com>
2016-01-03 20:29:08 -0500

    Added some empty functions for BST so I can implement in the future.

--- src/main/java/com/structures/trees/bst/BSTImpl.java
+++ src/main/java/com/structures/trees/bst/BSTImpl.java
@@ -2,32 +2,69 @@ package com.structures.trees.bst;
 
 public class BSTImpl {
 	BSTNode head;
-	
-	public BSTImpl(){}
-	
-	
-	public void insert(BSTNode node){
-		if(node != null){
-			
+
+	public BSTImpl() {
+	}
+
+	public void insert(BSTNode node) {
+		if (node != null) {
+
 		}
 	}
-	
-	public int getSize(){
+
+	public int getSize() {
 		int i = 0;
-		if(head != null){
-			
+		if (head != null) {
+
 		}
 		return i;
 	}
-	
-	public BSTNode breadthFirstSearch(BSTNode node){
-		return null;
-	}
-	
-	public BSTNode depthFirstSearch(BSTNode node){
-		return null;
-	}
-	
-	
 
+	public BSTNode breadthFirstSearch(BSTNode node) {
+		return null;
+	}
+
+	public BSTNode depthFirstSearch(BSTNode node) {
+		return null;
+	}
+
+	public int maxDepth(BSTNode node) {
+		return 0;
+	}
+
+	public int minValue() {
+		return 0;
+	}
+
+	public int maxValue() {
+		return 0;
+	}
+
+	public void printPostOrder() {
+
+	}
+
+	public boolean hasPathSum() {
+		return false;
+	}
+
+	public void printPaths() {
+
+	}
+
+	public void mirror() {
+
+	}
+
+	public void doubleTree() {
+
+	}
+
+	public boolean sameTree(BSTNode node) {
+		return false;
+	}
+
+	public boolean isBST() {
+		return false;
+	}
 }
